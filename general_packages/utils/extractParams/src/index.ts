f59ac5396b2047f693c7592983d6cf5e83bd45ff
RadoslavTsvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2025-06-23 02:01:42 +0300

    added support for the dsl of router path params types

--- general_packages/utils/extractParams/src/index.ts
+++ general_packages/utils/extractParams/src/index.ts
@@ -1,18 +1,56 @@
+type ContainsAtTheEnd<T extends string, Y extends string> = T extends `${infer Rest}${Y}` ? true : false;
+
+type k = ContainsAtTheEnd<"koko$", "$">  extends true ? number : string;
+
+
+
+
+type Alphabet = 
+  | "a" | "b" | "c" | "d" | "e" | "f" | "g"
+  | "h" | "i" | "j" | "k" | "l" | "m" | "n"
+  | "o" | "p" | "q" | "r" | "s" | "t" | "u"
+  | "v" | "w" | "x" | "y" | "z"
+  | "A" | "B" | "C" | "D" | "E" | "F" | "G"
+  | "H" | "I" | "J" | "K" | "L" | "M" | "N"
+  | "O" | "P" | "Q" | "R" | "S" | "T" | "U"
+  | "V" | "W" | "X" | "Y" | "Z";
+
+type RemoveNonAlphabetic<S extends string> =
+  S extends `${infer First}${infer Rest}`
+    ? First extends Alphabet
+      ? `${First}${RemoveNonAlphabetic<Rest>}`
+      : RemoveNonAlphabetic<Rest>
+    : "";
+
+
+
 export type ExtractParams<
   T extends string,
   ReturnType extends Record<string, string> = {}
 > = T extends `/${infer CurrentParam}`
   ? CurrentParam extends `:${infer ParamName}`
     ? ParamName extends `${infer Param}/${infer Rest}`
-      ? ExtractParams<`/${Rest}`, { [P in Param]: string } & { [P in keyof ReturnType]: ReturnType[P]}>
-      : {[p in keyof ReturnType]: string} & {[p in ParamName]: string} 
+      ? ExtractParams<
+      `/${Rest}`,
+       { 
+        [P in Param as RemoveNonAlphabetic<P>]: 
+        ContainsAtTheEnd<Param,  "$"> extends true 
+          ? number 
+          : ContainsAtTheEnd<Param, "("> extends true 
+            ? Date 
+            : ContainsAtTheEnd<Param, "^"> extends true 
+              ? boolean 
+              : string }
+           & 
+        { [P in keyof ReturnType]: ReturnType[P]}>
+      : {[P in keyof ReturnType]: ReturnType[P]} & {[p in ParamName]: string} 
     : CurrentParam extends `${infer NotDynamicParam}/${infer Rest}`
       ? ExtractParams<`/${Rest}`, ReturnType>
       : ReturnType
   : never 
 
 
-type j = ExtractParams<"/koko/lolo/userId/:product/:koko/lolo/:lolo/popo/:kiki">;
+type j = ExtractParams<"/koko/lolo/userId/:product^/:koko/lolo/:lolo$/popo/:kiki">;
 
 export function extractParams<T extends string>(path: T): ExtractParams<T> {
     return {} as ExtractParams<T>;
